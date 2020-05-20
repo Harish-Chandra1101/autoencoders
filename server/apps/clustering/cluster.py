83f8614b33e78e734dc71629c352af10f042136e
sumanth-bmsce <ssimha152@gmail.com>
2020-05-20 09:36:17 +0530

    Added Cluster class with kmeans function

---
+++ server/apps/clustering/cluster.py
@@ -0,0 +1,15 @@
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+
+
+class Cluster:
+    def __init__(self, data, cluster_config):
+        self.__data = data
+        self.__cluster_config = cluster_config
+
+    def kmeans(self):
+        config = self.__cluster_config['kmeans']
+        cluster_labels = KMeans(n_clusters=config['num_clusters']).fit_predict(self.__data)
+        return  cluster_labels
+
+    
