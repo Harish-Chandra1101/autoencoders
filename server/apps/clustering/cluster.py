cfaeb430c827abceaa0fa764e659d93aac064a6f
sumanth-bmsce <ssimha152@gmail.com>
2020-05-23 13:45:30 +0530

    Modified cluster.py to add docstring

--- server/apps/clustering/cluster.py
+++ server/apps/clustering/cluster.py
@@ -3,12 +3,23 @@ from sklearn.mixture import GaussianMixture
 
 
 class Cluster:
+    """
+    Performs clustering of the given data
+    """
     def __init__(self, data, cluster_config):
+        """
+        :param data: data from autoencoders feature extraction
+        :param cluster_config:
+        Sample cluster config json for kmeans
+        {
+            "num_clusters": 3
+        }
+        """
         self.__data = data
         self.__cluster_config = cluster_config
 
     def kmeans(self):
-        config = self.__cluster_config['kmeans']
-        cluster_labels = KMeans(n_clusters=config['num_clusters']).fit_predict(self.__data)
+        cluster_labels = KMeans(n_clusters=self.__cluster_config['num_clusters']).fit_predict(self.__data)
         return  cluster_labels
+
 
