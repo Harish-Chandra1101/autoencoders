81a013cf96c69457956c9190acd53ddf2ee7c659
sumanth-bmsce <ssimha152@gmail.com>
2020-05-23 14:38:17 +0530

    Modified autoencoder.py to get final parameters

--- server/apps/clustering/cluster.py
+++ server/apps/clustering/cluster.py
@@ -1,11 +1,13 @@
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
+from server.apps.logging.logger import ClusteringLogger
 
 
 class Cluster:
     """
     Performs clustering of the given data
     """
+
     def __init__(self, data, cluster_config):
         """
         :param data: data from autoencoders feature extraction
@@ -17,9 +19,15 @@ class Cluster:
         """
         self.__data = data
         self.__cluster_config = cluster_config
+        self.cluster_logger = None
 
     def kmeans(self):
+        self.cluster_logger = ClusteringLogger("kmeans")
+        self.cluster_logger.logger.info("Cluster Config Received is {}".format(self.__cluster_config))
+
         cluster_labels = KMeans(n_clusters=self.__cluster_config['num_clusters']).fit_predict(self.__data)
-        return  cluster_labels
 
+        self.cluster_logger.logger.info("Clustering completed")
+        self.cluster_logger.logger.info("Returning clustering labels")
 
+        return cluster_labels
